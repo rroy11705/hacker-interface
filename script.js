1bb8c63ea635cd0d93dcb24de0328c48587984af
rroy11705 <rroy11705@gmail.com>
2024-06-11 11:29:19 +0530

    feat: hacker interface

---
+++ script.js
@@ -0,0 +1,27 @@
+function callPopup() {
+    window.location.href = "/calling-popup.html"
+}
+
+function smsPopup() {
+    window.location.href = "/sms-popup.html"
+}
+
+function acceptCall() {
+    window.location.href = "/call-accepted.html"
+}
+
+function openSettings() {
+    window.location.href = "/settings.html"
+}
+
+function goToIndex() {
+    window.location.href = "/index.html"
+}
+
+function flightModeOn() {
+    window.location.href = "/settings-fm-on.html"
+}
+
+function flightModeOff() {
+    window.location.href = "/settings.html"
+}
